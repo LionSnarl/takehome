686d16406b60cd3660b73132d74a1e9f644c387b
andeeisaacs <andeefonder@gmail.com>
2021-03-23 15:19:24 -0700

    RC2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import Form from './Components/Form/Form'
-
-
-
 export default function App() {
-
   return (
     <>
-      <h1> Madlibs v1 (Dynamic form and inputs buildout) </h1>
+      <h1> Madlibs v1 (Dynamic form and inputs buildout) / (No styling) </h1>
       <hr/>
       <Form />
     </>
--- src/Components/Story/Story.js
+++ src/Components/Story/Story.js
@@ -1,9 +1,5 @@
 export default function Story({allState}) {
-
     // Dynamic ideas: Create state to decide which story is displayed based on how many items are in each parent key of the form state.
-
-
-
     return(
         <>
             <h2> Madlibs story componenet </h2>
@@ -11,7 +7,6 @@ export default function Story({allState}) {
                 {/* For sake of not generating a ton of stories, we're just going to display our passed state object down to the child.*/}
                 <h2> State object passed into child:</h2>
                 {JSON.stringify(allState)}
-
             </p>
         </>
     )

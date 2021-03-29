f49350cec42be5b6eab7254538b564f4e880f195
Ryan Sarll <lionsnarl1@gmail.com>
2021-03-28 23:00:53 -0700

    use stylings from Material UI

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ export default function App() {
   return (
     <>
       <h1> Madlibs v1 (Dynamic form and inputs buildout) / (No styling) </h1>
-      <h2> Add some Styling </h2>
+      <h2> Add some Styling here with possibly Material UI</h2>
       <hr/>
       <Form />
     </>
